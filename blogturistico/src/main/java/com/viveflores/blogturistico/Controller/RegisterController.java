520ded7a062bd06325f9fd2abbabc1b7be132185
JerryCastro-2023536 <jcastro-2023536@kinal.edu.gt>
2026-04-21 11:53:54 -0600

    feat: Se agrego el metodo de registro

--- blogturistico/src/main/java/com/viveflores/blogturistico/Controller/RegisterController.java
+++ blogturistico/src/main/java/com/viveflores/blogturistico/Controller/RegisterController.java
@@ -1,13 +1,54 @@
 package com.viveflores.blogturistico.Controller;
 
+import ch.qos.logback.core.model.Model;
+import com.viveflores.blogturistico.Entity.Usuarios;
+import com.viveflores.blogturistico.Service.UsuariosService;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.time.LocalDate;
 
 @Controller
 public class RegisterController {
+
+    private final UsuariosService usuariosService;
+
+    public RegisterController(UsuariosService usuariosService) {
+        this.usuariosService = usuariosService;
+    }
 
     @GetMapping("/registro")
     public String register(){
         return "register";
     }
+
+    @PostMapping("/crear")
+    public String crearCuenta(@RequestParam("username") String username,
+                              @RequestParam("nombre")  String nombre,
+                              @RequestParam("apellido") String apellido,
+                              @RequestParam("email") String email,
+                              @RequestParam("contrasena") String contrasena,
+                              Model model){
+
+        Usuarios u = new Usuarios();
+
+        u.setUsername(username);
+        u.setNombre_usuario(nombre);
+        u.setApellido_usuario(apellido);
+        u.setEmail_usuario(email);
+        u.setContrasena_usuario(contrasena);
+        u.setRol("usuario");
+        u.setFecha_registro(LocalDate.now());
+
+        usuariosService.saveUsuarios(u);
+
+
+
+
+        return "redirect:/acceder";
+    }
+
 }
