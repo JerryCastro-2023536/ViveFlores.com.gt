8d936b83a0f873e00a518a99abc1a87139712e61
JerryCastro-2023536 <jcastro-2023536@kinal.edu.gt>
2026-04-21 11:54:47 -0600

    feat: Se agregado el controlador de la vista

---
+++ blogturistico/src/main/java/com/viveflores/blogturistico/Controller/ViewController.java
@@ -0,0 +1,17 @@
+package com.viveflores.blogturistico.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ViewController {
+    @GetMapping("/quienessomos")
+    public String quienesSomos(){
+        return "QuienesSomos";
+    }
+
+    @GetMapping("/contacto")
+    public String contacto(){
+        return "Contacto";
+    }
+}
