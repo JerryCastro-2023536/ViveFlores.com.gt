2250f9599fc1e1d8901f8a4673d3a80fee1067bc
JerryCastro-2023536 <jcastro-2023536@kinal.edu.gt>
2026-04-21 11:55:55 -0600

    feat: Se agregado el controllador del login

--- blogturistico/src/main/java/com/viveflores/blogturistico/Controller/LoginController.java
+++ blogturistico/src/main/java/com/viveflores/blogturistico/Controller/LoginController.java
@@ -1,13 +1,43 @@
 package com.viveflores.blogturistico.Controller;
 
+import com.viveflores.blogturistico.Entity.Usuarios;
+import com.viveflores.blogturistico.Service.UsuariosService;
+import jakarta.servlet.http.HttpSession;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class LoginController {
 
+    private final UsuariosService usuariosService;
+
+    public LoginController(UsuariosService usuariosService) {
+        this.usuariosService = usuariosService;
+    }
+
     @GetMapping("/acceder")
     public String login(){
         return "login";
+    }
+
+    @PostMapping("/login")
+    public String iniciarSesion(@RequestParam("username") String username,
+                                @RequestParam("contrasena") String contraena,
+                                Model model,
+                                HttpSession session){
+
+        Usuarios u = usuariosService.login(username, contraena);
+
+        if (u != null) {
+            session.setAttribute("usuarioLogueado", u);
+            return "redirect:/principal";
+        } else {
+            model.addAttribute("error", "Credenciales incorrectas");
+            return "login";
+        }
     }
 }
