e6d6b62000101e23e94b9f07dc4da8fb69c90b0e
diegocartajena-2022510 <dcartajena-2022510@kinal.edu.gt>
2026-04-22 11:19:03 -0600

    feat: se agregaron metodos agregar,eliminar y actualizar

--- blogturistico/src/main/java/com/viveflores/blogturistico/Service/UsuariosService.java
+++ blogturistico/src/main/java/com/viveflores/blogturistico/Service/UsuariosService.java
@@ -9,4 +9,7 @@ import com.viveflores.blogturistico.Entity.Usuarios;
 @Service
 public interface UsuariosService {
     List<Usuarios> listar();
+    Usuarios guardar(Usuarios usuarios);
+    Usuarios buscarPorId(int id);
+    void eliminar(int id);
 }
