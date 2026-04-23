6a93ff22ccacdf012ce7a76fcb23dc7318119ffb
diegocartajena-2022510 <dcartajena-2022510@kinal.edu.gt>
2026-04-22 19:27:09 -0600

    feat: se agrego implementacion servicios de crud

--- blogturistico/src/main/java/com/viveflores/blogturistico/Service/SolicitudPublicacionServiceImplements.java
+++ blogturistico/src/main/java/com/viveflores/blogturistico/Service/SolicitudPublicacionServiceImplements.java
@@ -1,45 +1,38 @@
 package com.viveflores.blogturistico.Service;
 
 import com.viveflores.blogturistico.Entity.SolicitudPublicacion;
-import com.viveflores.blogturistico.Exception.FechasValidar;
-import com.viveflores.blogturistico.Exception.NotFoundExcepcion;
 import com.viveflores.blogturistico.Repository.SolicitudPublicacionRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
 public class SolicitudPublicacionServiceImplements implements SolicitudPublicacionService{
-    private final SolicitudPublicacionRepository SolicitudPublicacionRepository;
-    FechasValidar fv = new FechasValidar();
 
-    public SolicitudPublicacionServiceImplements(SolicitudPublicacionRepository solicitudPublicacionRepository) {
-        SolicitudPublicacionRepository = solicitudPublicacionRepository;
+    @Autowired
+    private SolicitudPublicacionRepository repo;
+
+    @Override
+    public List<SolicitudPublicacion> listar(){
+        return repo.findAll();
     }
-
-
     @Override
-    public List<SolicitudPublicacion> getAllSolicitud() {return SolicitudPublicacionRepository.findAll();}
-
-    @Override
-    public SolicitudPublicacion getSolicitudById(Integer id) { return SolicitudPublicacionRepository.findById(id).orElseThrow(() ->
-            new NotFoundExcepcion("El id no existe"));}
-
-    @Override
-    public SolicitudPublicacion saveSolicitud(SolicitudPublicacion solicitudPublicacion) throws RuntimeException {
-        fv.validarLocalDate(solicitudPublicacion.getFecha_solicitud());
-        return SolicitudPublicacionRepository.save(solicitudPublicacion);
+    public SolicitudPublicacion guardar(SolicitudPublicacion solicitudPublicacion) {
+       return repo.save(solicitudPublicacion);
     }
 
     @Override
-    public SolicitudPublicacion updateSolicitud(Integer id, SolicitudPublicacion SolicitudPublicacion) {
-        return SolicitudPublicacionRepository.save(SolicitudPublicacion);
+    public SolicitudPublicacion buscarPorId(int id) {
+        return repo.findById(id).orElse(null);
     }
 
     @Override
-    public void deleteSolicitud(Integer id) {
-        SolicitudPublicacionRepository.deleteById(id);
+    public void eliminar(int id) {
+        repo.deleteById(id);
     }
+    
 
 
 }
