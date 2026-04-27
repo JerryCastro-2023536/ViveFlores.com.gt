0934c69cf87d709059e4d30d94b9eb81ba371bda
diegocartajena-2022510 <dcartajena-2022510@kinal.edu.gt>
2026-04-26 20:10:14 -0600

    feat: ser crearon metodos crud

--- blogturistico/src/main/java/com/viveflores/blogturistico/Controller/PublicacionesController.java
+++ blogturistico/src/main/java/com/viveflores/blogturistico/Controller/PublicacionesController.java
@@ -2,64 +2,94 @@ package com.viveflores.blogturistico.Controller;
 
 import com.viveflores.blogturistico.Entity.Publicaciones;
 import com.viveflores.blogturistico.Service.PublicacionesService;
-import jakarta.validation.Valid;
-import org.springframework.http.HttpStatus;
+
+import java.io.IOException;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
-import java.util.List;
+import com.viveflores.blogturistico.Repository.PublicacionesRepository;
 
-@RestController
-@RequestMapping("/api/publicaciones")
+@Controller
 public class PublicacionesController {
-    private final PublicacionesService publicacionesService;
+    @Autowired
+    PublicacionesService service;
 
-    public PublicacionesController(PublicacionesService publicacionesService) {
-        this.publicacionesService = publicacionesService;
-    }
+    @Autowired
+    PublicacionesRepository repo;
 
-    @GetMapping
-    List<Publicaciones> getAllPublicaciones() {
-        return publicacionesService.getAllPublicaciones();
-    }
+    @GetMapping("/")
+    public String inicio() {return "redirect:/publicaciones";}
 
-    @GetMapping("/{id}")
-    public ResponseEntity<Object> getPublicacionesById(@PathVariable Integer id) {
-        try {
-            Publicaciones buscarPublicaciones = publicacionesService.getPublicacionesById(id);
-            return new ResponseEntity<>(buscarPublicaciones, HttpStatus.FOUND);
-        } catch (Exception e) {
-            return ResponseEntity.badRequest().body(e.getMessage());
+    @GetMapping("/publicaciones")
+    public String listarPublicaciones(@RequestParam(name = "accion", required = false) String accion,
+                                 @RequestParam(name = "id", required = false) Integer id,
+                                 Model model) {
+
+        model.addAttribute("publicaciones",service.listar());
+        model.addAttribute("accion", accion);
+
+        if("editar".equals(accion) && id !=null ){
+            model.addAttribute("uEncontrado",service.buscarporId(id));
+        }else{
+            model.addAttribute("uEcontrado", new Publicaciones());
         }
+        return "publicaciones";
     }
 
-    @PostMapping
-    public ResponseEntity<Object> savePublicaciones(@Valid @RequestBody Publicaciones publicaciones) {
-        try {
-            Publicaciones savedPublicaciones = publicacionesService.savePublicaciones(publicaciones);
-            return new ResponseEntity<>(savedPublicaciones, HttpStatus.CREATED);
-        } catch (Exception e) {
-            return ResponseEntity.badRequest().body(e.getMessage());
+     @GetMapping("/foto/{id}")
+    public ResponseEntity<byte[]> verFoto(@PathVariable int id) {
+        Publicaciones p = service.buscarporId(id);
+        if (p != null && p.getFoto() != null) {
+            return ResponseEntity.ok()
+                    .contentType(MediaType.IMAGE_JPEG)
+                    .body(p.getFoto());
         }
+        return ResponseEntity.notFound().build();
     }
 
-    @PutMapping("/{id}")
-    public ResponseEntity<Object> updatePublicaciones(@PathVariable Integer id, @Valid @RequestBody Publicaciones publicaciones) {
-        try {
-            Publicaciones actualizarPublicaciones = publicacionesService.updatePublicaciones(id, publicaciones);
-            return new ResponseEntity<>(actualizarPublicaciones, HttpStatus.OK);
-        } catch (Exception e) {
-            return ResponseEntity.badRequest().body(e.getMessage());
+    @PostMapping("/publicaciones/agregar")
+    public String guardar(@ModelAttribute Publicaciones publicacion, 
+                      @RequestParam("foto") MultipartFile archivo) throws IOException {
+        if (!archivo.isEmpty()) {
+            publicacion.setFoto(archivo.getBytes());
         }
+        repo.save(publicacion);
+        return "redirect:/publicaciones";
     }
 
-    @DeleteMapping("/{id}")
-    public ResponseEntity<Object> deletePublicaciones(@PathVariable Integer id){
-        try {
-            publicacionesService.deletePublicaciones(id);
-            return new ResponseEntity<>(HttpStatus.OK);
-        } catch (Exception e) {
-            return ResponseEntity.badRequest().body(e.getMessage());
+    @PostMapping("/publicaciones/buscar")
+    public String buscar(@RequestParam Integer id_publicacion, Model model) {
+        if (id_publicacion != null) {
+            Publicaciones u = service.buscarporId(id_publicacion);
+            if (u != null) {
+                model.addAttribute("publicaciones", java.util.List.of(u));
+            } else {
+                model.addAttribute("publicaciones", service.listar());
+                model.addAttribute("error", "No existe ese ID");
+            }
         }
+        return "publicaciones";
     }
+
+
+    @PostMapping("/publicaciones/editar")
+    public String editar(@ModelAttribute Publicaciones publicaciones) {
+        service.guardar(publicaciones);
+        return "redirect:/publicaciones";
+    }
+
+    @GetMapping("/publicaciones/eliminar/{id}")
+    public String eliminarUsuario(@PathVariable int id) {
+        service.eliminar(id);
+        return "redirect:/publicaciones";
+    }
+
+    
 }
