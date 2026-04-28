63e551f8831c8d106895fd18c1f1dce194a77649
diegocartajena-2022510 <dcartajena-2022510@kinal.edu.gt>
2026-04-27 20:30:53 -0600

    refactor: se cambio el tipo de variable de foto

--- blogturistico/src/main/java/com/viveflores/blogturistico/Entity/Publicaciones.java
+++ blogturistico/src/main/java/com/viveflores/blogturistico/Entity/Publicaciones.java
@@ -1,10 +1,16 @@
 package com.viveflores.blogturistico.Entity;
 
-import jakarta.persistence.*;
+import java.time.LocalDate;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Lob;
+import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
-
-import java.time.LocalDate;
 
 @Entity
 @Table(name = "publicaciones")
@@ -27,7 +33,7 @@ public class Publicaciones {
     private String direccion;
 
     @Lob
-    @Column(name = "foto", columnDefinition = "blob")
+    @Column(name = "foto", columnDefinition = "longblob")
     private byte[] foto;
 
     @NotNull(message = "El campo no debe ir vacio")
