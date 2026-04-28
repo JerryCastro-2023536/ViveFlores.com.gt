2f3e0147d7fcc3106d02306eb774840be1f844ec
diegocartajena-2022510 <dcartajena-2022510@kinal.edu.gt>
2026-04-27 20:30:29 -0600

    fix: se arreglo el listar tabla

--- blogturistico/src/main/java/com/viveflores/blogturistico/Controller/PublicacionesController.java
+++ blogturistico/src/main/java/com/viveflores/blogturistico/Controller/PublicacionesController.java
@@ -1,20 +1,22 @@
 package com.viveflores.blogturistico.Controller;
 
-import com.viveflores.blogturistico.Entity.Publicaciones;
-import com.viveflores.blogturistico.Service.PublicacionesService;
-
 import java.io.IOException;
-
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 
+import com.viveflores.blogturistico.Entity.Publicaciones;
 import com.viveflores.blogturistico.Repository.PublicacionesRepository;
+import com.viveflores.blogturistico.Service.PublicacionesService;
 
 @Controller
 public class PublicacionesController {
@@ -43,7 +45,7 @@ public class PublicacionesController {
         return "publicaciones";
     }
 
-     @GetMapping("/foto/{id}")
+     @GetMapping("publicaciones/foto/{id}")
     public ResponseEntity<byte[]> verFoto(@PathVariable int id) {
         Publicaciones p = service.buscarporId(id);
         if (p != null && p.getFoto() != null) {
@@ -56,11 +58,11 @@ public class PublicacionesController {
 
     @PostMapping("/publicaciones/agregar")
     public String guardar(@ModelAttribute Publicaciones publicacion, 
-                      @RequestParam("foto") MultipartFile archivo) throws IOException {
+                      @RequestParam("fotoArchivo") MultipartFile archivo) throws IOException {
         if (!archivo.isEmpty()) {
             publicacion.setFoto(archivo.getBytes());
         }
-        repo.save(publicacion);
+        service.guardar(publicacion);
         return "redirect:/publicaciones";
     }
 
@@ -80,11 +82,21 @@ public class PublicacionesController {
 
 
     @PostMapping("/publicaciones/editar")
-    public String editar(@ModelAttribute Publicaciones publicaciones) {
+    public String editar(@ModelAttribute Publicaciones publicaciones,
+                     @RequestParam("fotoArchivo") MultipartFile archivo) throws IOException {
+    
+        if (!archivo.isEmpty()) {
+            publicaciones.setFoto(archivo.getBytes());
+        } else {
+            Publicaciones temp = service.buscarporId(publicaciones.getId_publicacion());
+            if (temp != null) {
+                publicaciones.setFoto(temp.getFoto());
+            }
+        }
+    
         service.guardar(publicaciones);
         return "redirect:/publicaciones";
     }
-
     @GetMapping("/publicaciones/eliminar/{id}")
     public String eliminarUsuario(@PathVariable int id) {
         service.eliminar(id);
