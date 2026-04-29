289e261335509eebac1612a433be76aa2023e0e4
JerryCastro-2023536 <jcastro-2023536@kinal.edu.gt>
2026-04-29 10:31:37 -0600

    feat: Se agrego los metodos para renderizar por el id

--- blogturistico/src/main/java/com/viveflores/blogturistico/Controller/CardsController.java
+++ blogturistico/src/main/java/com/viveflores/blogturistico/Controller/CardsController.java
@@ -1,13 +1,18 @@
 package com.viveflores.blogturistico.Controller;
 
+import com.viveflores.blogturistico.Entity.Evento;
+import com.viveflores.blogturistico.Entity.Publicaciones;
+import com.viveflores.blogturistico.Entity.Servicio;
 import com.viveflores.blogturistico.Repository.EventoRepository;
 import com.viveflores.blogturistico.Repository.PublicacionesRepository;
 import com.viveflores.blogturistico.Service.EventoService;
 import com.viveflores.blogturistico.Service.PublicacionesService;
 import com.viveflores.blogturistico.Service.ServicioService;
 import org.springframework.stereotype.Controller;
+import org.springframework.stereotype.Service;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 public class CardsController {
@@ -27,16 +32,37 @@ public class CardsController {
         return "Eventos";
     }
 
+    @GetMapping("/evento/{id}")
+    public String verEvento(@PathVariable Integer id, Model model){
+        Evento evento = eventoService.getEventoById(id);
+        model.addAttribute("evento", evento);
+        return "evento";
+    }
+
     @GetMapping("/publicacionesAll")
     public String mostrarPublicaciones(Model model){
         model.addAttribute("publicaciones", publicacionesService.getAllPublicaciones());
         return "Publicaciones";
     }
 
+    @GetMapping("/post/{id}")
+    public String verPost(@PathVariable Integer id, Model model) {
+        Publicaciones post = publicacionesService.getPublicacionesById(id);
+        model.addAttribute("post", post);
+        return "post";
+    }
+
     @GetMapping("/serviciosAll")
     public String mostrarServicios(Model model){
         model.addAttribute("servicios", servicioService.getAllServicios());
         return "Servicios";
+    }
+
+    @GetMapping("/servicio/{id}")
+    public String verServico(@PathVariable Integer id, Model model){
+        Servicio servicio = servicioService.getServicioById(id);
+        model.addAttribute("servicio", servicio);
+        return "servicio";
     }
 
 }
