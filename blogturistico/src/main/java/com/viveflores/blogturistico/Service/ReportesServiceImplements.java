2391aca7ef2341a6714a8406cfd603ded3a02251
diegocartajena-2022510 <dcartajena-2022510@kinal.edu.gt>
2026-05-05 10:39:33 -0600

    feat: se hizo implementacion servicio

--- blogturistico/src/main/java/com/viveflores/blogturistico/Service/ReportesServiceImplements.java
+++ blogturistico/src/main/java/com/viveflores/blogturistico/Service/ReportesServiceImplements.java
@@ -4,55 +4,33 @@ import com.viveflores.blogturistico.Entity.Reportes;
 import com.viveflores.blogturistico.Exception.FechasValidar;
 import com.viveflores.blogturistico.Exception.NotFoundExcepcion;
 import com.viveflores.blogturistico.Repository.ReportesRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
 public class ReportesServiceImplements implements ReportesService{
+    @Autowired
+    private ReportesRepository repo;
 
-    private final ReportesRepository reportesRepository;
-    FechasValidar fv = new FechasValidar();
-
-    public ReportesServiceImplements(ReportesRepository reportesRepository) {
-        this.reportesRepository = reportesRepository;
+    @Override
+    public List<Reportes> listarReportes() {
+        return List.of();
     }
 
     @Override
-    public List<Reportes> getAllReportes() {
-        return reportesRepository.findAll();
+    public Reportes agregarReportes(Reportes reportes) {
+        return repo.save(reportes);
     }
 
     @Override
-    public Reportes getReporteById(Integer id) {
-        return reportesRepository.findById(id).orElseThrow(() ->
-                new NotFoundExcepcion("El id no existe"));
+    public Reportes BuscarPorId(int id) {
+        return null;
     }
 
     @Override
-    public Reportes saveReportes(Reportes reportes) {
-        fv.validarLocalDateTime(reportes.getFecha_envio());
-        return reportesRepository.save(reportes);
-    }
+    public void eliminarReportes(int id) {
 
-    @Override
-    public Reportes updateReportes(Integer id, Reportes reportes) {
-        Reportes reportes1 = reportesRepository.findById(id).orElseThrow(() ->
-                new NotFoundExcepcion("El id no existe"));;
-
-        reportes1.setAsunto(reportes.getAsunto());
-        reportes1.setMensaje(reportes.getMensaje());
-        reportes1.setFecha_envio(reportes.getFecha_envio());
-        reportes1.setId_usuario(reportes.getId_usuario());
-        fv.validarLocalDateTime(reportes1.getFecha_envio());
-
-        return reportesRepository.save(reportes1);
-    }
-
-    @Override
-    public void deleteReportes(Integer id) {
-        Reportes reportes = reportesRepository.findById(id).orElseThrow(() ->
-                new NotFoundExcepcion("El id no existe"));;
-        reportesRepository.delete(reportes);
     }
 }
