953a1f062cd841d829e27bb007c66289b349e476
JerryCastro <jcastro-2023536@kinal.edu.gt>
2026-05-04 20:42:27 -0600

    feat: se agrego una consulta Query dentro del repository para buscar el idPublicacion

--- blogturistico/src/main/java/com/viveflores/blogturistico/Repository/ResenaRepository.java
+++ blogturistico/src/main/java/com/viveflores/blogturistico/Repository/ResenaRepository.java
@@ -4,6 +4,14 @@ import com.viveflores.blogturistico.Entity.Resena;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+
 @Repository
 public interface ResenaRepository extends JpaRepository<Resena, Integer> {
+    
+    @Query("SELECT r FROM Resena r WHERE r.id_publicacion = :idPublicacion")
+    List<Resena> findByIdPublicacion(@Param("idPublicacion") Integer idPublicacion);
 }
