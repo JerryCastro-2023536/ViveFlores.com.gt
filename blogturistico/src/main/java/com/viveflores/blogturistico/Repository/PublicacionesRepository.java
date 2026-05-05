6a96473ffe4127428b86e2b113d7f424ed07802c
JerryCastro-2023536 <jcastro-2023536@kinal.edu.gt>
2026-05-05 11:38:54 -0600

    feat: Se agrego una consulta para buscar por nombre

--- blogturistico/src/main/java/com/viveflores/blogturistico/Repository/PublicacionesRepository.java
+++ blogturistico/src/main/java/com/viveflores/blogturistico/Repository/PublicacionesRepository.java
@@ -2,8 +2,14 @@ package com.viveflores.blogturistico.Repository;
 
 import com.viveflores.blogturistico.Entity.Publicaciones;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface PublicacionesRepository extends JpaRepository <Publicaciones, Integer> {
+    @Query("SELECT p FROM Publicaciones p WHERE LOWER(p.nombre_publicacion) LIKE LOWER(CONCAT('%', :nombre, '%'))")
+    List<Publicaciones> buscarPorNombre(@Param("nombre") String nombre);
 }
