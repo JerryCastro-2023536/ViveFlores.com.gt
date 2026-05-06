0dfbbf5de70187e7e6d1474598259b3ff05a7ac7
JerryCastro-2023536 <jcastro-2023536@kinal.edu.gt>
2026-05-06 11:43:25 -0600

    style: Se corrigio el indentado

--- blogturistico/src/main/java/com/viveflores/blogturistico/Controller/PerfilUsuarioController.java
+++ blogturistico/src/main/java/com/viveflores/blogturistico/Controller/PerfilUsuarioController.java
@@ -1,8 +1,12 @@
 package com.viveflores.blogturistico.Controller;
 
+import com.viveflores.blogturistico.Entity.Favoritos;
+import com.viveflores.blogturistico.Service.FavoritosService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
 
 @Controller
 public class PerfilUsuarioController {
