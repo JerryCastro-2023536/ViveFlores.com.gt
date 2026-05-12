1df05ae41465633f75042d955550842ebb423e7a
diegocartajena-2022510 <dcartajena-2022510@kinal.edu.gt>
2026-05-12 17:13:20 -0600

    feat: se agrego controladore de adminreportes

---
+++ blogturistico/src/main/java/com/viveflores/blogturistico/Controller/reportesAdminController.java
@@ -0,0 +1,33 @@
+package com.viveflores.blogturistico.Controller;
+
+import com.viveflores.blogturistico.Entity.Reportes;
+import com.viveflores.blogturistico.Service.ReportesService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
+
+@Controller
+public class reportesAdminController {
+
+    @Autowired
+    private ReportesService reportesService;
+
+    @GetMapping("/")
+    public String index(Model model) {
+        List<Reportes> lista = reportesService.listarReportes();
+        model.addAttribute("reportes", lista);
+        return "reportesAdmin";
+    }
+    @GetMapping("/eliminar/{id}")
+    public String eliminar(@PathVariable("id") int id) {
+        reportesService.eliminarReportes(id);
+        return "redirect:/";
+    }
+    @GetMapping("/nuevo")
+    public String nuevo(Model model) {
+        model.addAttribute("reporte", new Reportes());
+        return "reportesUsuario";
+    }
+}
